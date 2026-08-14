8de0effb8c820fe9a03c2ee57be9dd226987214b
KheeraKing <kmaaranu@student.fullsail.edu>
2026-08-14 01:27:23 -0500

    DAY 1: basics
    - strings
    - concatenation
    - input
    - building a small calculator that outputs a string including your age in decades

---
+++ PythonCourse/greetings.py
@@ -0,0 +1,34 @@
+
+# HEADER: Learning Tax Calculations
+# amount = 200
+# tax = .07
+# total = int(amount + amount*tax)
+# print (total)
+
+#-------------------------
+# HEADER: Learning Strings 
+# name = 'Sarah'
+# print(name)
+
+# store_name = "Sarah's Store"
+# print(store_name)
+
+#-------------------------
+# HEADER: Learning String Concatenation 
+
+# hello = "Hello"
+# name = "Sarah"
+# greeting = hello + " " + name
+# print(greeting)
+
+#-------------------------
+# HEADER: Learning Input
+
+# hello = "Hello"
+# name = input("What's your name?\n")
+# greeting = hello + " " + name
+# print(greeting)
+
+#-------------------------
+# TODO: Creating age calculator
+
